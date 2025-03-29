448925c1cc7a90fa1f06f90bf7f79bcba4c67653
himani1001 <himanisharma15435029@gmail.com>
2025-03-29 18:09:57 +0530

    javascript completed

--- Weather API/script.js
+++ Weather API/script.js
@@ -10,7 +10,33 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 
 let dateObj = new Date();
 let month = months[dateObj.getUTCMonth()];
-let day = dateObj.getUTCDate() - 1;
+let day = dateObj.getUTCDate();
 let year = dateObj.getUTCFullYear();
 
 date.innerHTML = `${month} ${day} ${year}`;
+
+const app = document.getElementById('app');
+
+const getWeather = async() => {
+    try{
+
+        const cityName = document.getElementById('searchBarInput').value;
+        const weatherDataFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7ff7f6edc82d5b42cb41100b21681f66&units=metric`, 
+                {    headers: {
+                        Accept: 'application/json'
+                } 
+        });
+
+        const weatherData = await weatherDataFetch.json();
+        console.log(weatherData);
+        city.innerHTML = `${weatherData.name}`;
+        description.innerHTML = `${weatherData.weather[0].main}`;
+        tempImg.innerHTML = `<img src = "https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png" />`;
+        temp.innerHTML = `<h2>${Math.round(weatherData.main.temp)}°C</h2>`;
+        tempMax.innerHTML = `${weatherData.main.temp_max}°C`;
+        tempMin.innerHTML = `${weatherData.main.temp_min}°C`;
+    }
+    catch(error){
+        console.log(error);
+    }
+}
